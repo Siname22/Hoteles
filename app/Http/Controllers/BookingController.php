7f425a4f5c6e3b948b2ba4916bd52ab4017061ce
Iván García <igarciacenalmor@educa.madrid.org>
2023-01-16 21:33:19 +0100

    Controlador creado

---
+++ app/Http/Controllers/BookingController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Booking;
+use Illuminate\Http\Request;
+
+class BookingController extends Controller
+{
+
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(Booking $booking)
+    {
+        //
+    }
+
+    public function edit(Booking $booking)
+    {
+        //
+    }
+
+    public function update(Request $request, Booking $booking)
+    {
+        //
+    }
+
+    public function destroy(Booking $booking)
+    {
+        //
+    }
+}
