a999d0234dd5652a9eb33156410a2724e4cd668e
esteban <esteban>
2023-01-24 12:36:57 +0100

    CSS, con los colors

--- resources/views/paginas/inicio/inicio.blade.php
+++ resources/views/paginas/inicio/inicio.blade.php
@@ -4,17 +4,33 @@
 
 <html>
 
-    <div>
-        <img src="logo.png">
-    </div>
-    <div>
-        <form action="login">
-            <input type="submit"  value="Log in">
-        </form>
-        <form action="sign_up">
-            <input type="submit" value="Sign up">
-        </form>
+    <head>
 
-    </div>
+        <link rel='stylesheet' href='/css/Styles.css'>
+
+    </head>
+
+    <body>
+
+        <div id='div_logo'>
+            <a href='' id='div_logo'>
+                <img src="/img/logofinal.png" id='logo' alt='logo M2H' title='M2H logo'>
+            </a>
+        </div>
+        <br/>
+        <br/>
+        <br/>
+        <div id='box_form_buttons'>
+            <p></p>
+            <form action="login">
+                <input type="submit"  value="Log In" id='login'>
+            </form>
+            <form action="sign_up">
+                <input type="submit" value="Sign Up" id='signup'>
+            </form>
+
+        </div>
+
+    </body>
 
 </html>
